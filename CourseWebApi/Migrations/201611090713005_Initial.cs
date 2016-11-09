94bf5eeb7f54dea4447c26dbd7d7d540eaf3ee6e
Raghu1986 <Raghu1986@outlook.com>
2016-11-09 12:51:36 +0530

    Course List Web api created

---
+++ CourseWebApi/Migrations/201611090713005_Initial.cs
@@ -0,0 +1,27 @@
+namespace CourseWebApi.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Courses",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        CourseType = c.String(),
+                        LogoUri = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Courses");
+        }
+    }
+}
---
+++ CourseWebApi/Models/Course.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CourseWebApi.Models
+{
+    public class Course
+    {
+        [Key]
+
+        public int Id { get; set; }
+
+        public string CourseType { get; set; }
+        public string LogoUri { get; set; }
+
+    }
+}
